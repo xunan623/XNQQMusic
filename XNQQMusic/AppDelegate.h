23f9e1bdfbd20c33fc459a9ed3893cb06dacc123
xunan <xunan623@163.com>
2017-01-17 14:21:38 +0800

    Initial Commit

---
+++ XNQQMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XNQQMusic
+//
+//  Created by xunan on 2017/1/17.
+//  Copyright © 2017年 xunan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XNQQMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XNQQMusic
+//
+//  Created by xunan on 2017/1/17.
+//  Copyright © 2017年 xunan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
